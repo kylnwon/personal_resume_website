31bbabfa31817de42180db6b6cf15f2a45fb28f1
Kyle <Kyle@MacBook-Pro.local>
2022-08-29 21:05:55 -0700

    v1.1

--- pages/index.js
+++ pages/index.js
@@ -55,13 +55,6 @@ export default function Home() {
         <title>{data.name}</title>
       </Head>
       {/* This button should not go into production */}
-      {process.env.NODE_ENV === "development" && (
-        <div className="fixed bottom-5 right-5">
-          <Link href="/edit">
-            <Button type="primary">Edit Data</Button>
-          </Link>
-        </div>
-      )}
       <div className="gradient-circle"></div>
       <div className="gradient-circle-bottom"></div>
 
@@ -110,18 +103,6 @@ export default function Home() {
                 name={project.title}
                 description={project.description}
                 onClick={() => window.open(project.url)}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
-          <h1 className="tablet:m-10 text-2xl text-bold">Services.</h1>
-          <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
-            {data.services.map((service, index) => (
-              <ServiceCard
-                key={index}
-                name={service.title}
-                description={service.description}
               />
             ))}
           </div>
